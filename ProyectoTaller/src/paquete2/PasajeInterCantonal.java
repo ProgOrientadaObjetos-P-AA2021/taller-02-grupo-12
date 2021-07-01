7323a430de0e5e5bf79ade65ebc68c70337fc8e6
eddy2819 <camiloguarnizo2819@gmail.com>
2021-07-01 15:50:14 -0500

    cambio

--- ProyectoTaller/src/paquete2/PasajeInterCantonal.java
+++ ProyectoTaller/src/paquete2/PasajeInterCantonal.java
@@ -1,13 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package paquete2;
 
 import java.io.Serializable;
 
+/**
+ *
+ * @author reroes
+ */
 public abstract class PasajeInterCantonal implements Serializable{
     
+    protected String nombre;
+    protected String cedula;
+    protected String origen;
+    protected String destino;
+    protected int km;
+    protected  double preBase;
+    protected double vaPasaje;
     
+    public PasajeInterCantonal(String n,String ci,String o,String de,int k,double pre){
+        nombre = n;
+        cedula = ci;
+        origen =  o;
+        destino = de;
+        km = k;
+        preBase = pre;
+    }
     
+    public void setNombrePasajero(String n){
+        nombre = n;
+        
+    }
+    public void setCedula(String i){
+        cedula = i;
+    }
+    public void setOrigen(String v){
+        origen = v;
+    }
+    public void setDestino(String s){
+        destino = s;
+    }
+    public void set(int d){
+        km = d;
+    }
+    public void setTarifaBase(double t){
+        preBase = t;
+    }
+    
+    public String getNombrePasjero(){
+        return nombre;
+    }
+    public String getCedula(){
+        return cedula;
+    }
+    public String getOrigen(){
+        return origen;
+    }
+    public String getDestino(){
+        return destino;
+    }
+    public int getNKmDistancia(){
+        return km;
+    }
+    public double getTarifaBase(){
+        return preBase;
+    }
+    
+  
     @Override
     public String toString(){
         return String.format("Pasaje.");
-    }      
+    }
+    
+    
 }
